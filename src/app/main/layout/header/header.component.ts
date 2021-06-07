0fb433c142c5ec7fe4f56bd4ecba0678f52252e5
66redBastard <66redbastard@gmail.com>
2021-06-07 16:08:55 +0300

    header on scroll changes styles, TODO: add logo for mobile, simple svg+keframes star animation

--- src/app/main/layout/header/header.component.ts
+++ src/app/main/layout/header/header.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  HostListener,
+  Inject,
+  OnDestroy,
+} from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 import { LayoutService } from '../services/layout.service';
 import { User } from '../../../auth/models/user';
 import { AccountService } from '../../../auth/services/account.service';
@@ -10,14 +17,17 @@ import { AccountService } from '../../../auth/services/account.service';
 })
 export class HeaderComponent implements OnInit {
   isMenuOpened: boolean;
+  currentPosition;
 
   user: User;
 
   constructor(
+    @Inject(DOCUMENT) private _document: Document,
     private layoutservice: LayoutService,
     private accountService: AccountService,
   ) {
     this.accountService.user.subscribe((x) => (this.user = x));
+    this._document.addEventListener('scroll', this.onContentScrolled);
   }
 
   ngOnInit() {
@@ -32,4 +42,34 @@ export class HeaderComponent implements OnInit {
   logout() {
     this.accountService.logout();
   }
+
+  OnDestroy() {
+    this._document.removeEventListener('scroll', this.onContentScrolled);
+  }
+
+  onContentScrolled = (e) => {
+    let scroll = window.pageYOffset;
+    let header = document.getElementById('header-main-page');
+    let logoText = document.getElementById('logo-text');
+    let navLinks = document.getElementById('nav-links-container');
+    // const elements = document.getElementsByClassName('nav-link');
+
+    if (scroll > this.currentPosition) {
+      console.log('scrollDown');
+
+      header.classList.add('header--onscroll-down');
+      logoText.classList.add('text-dark');
+      navLinks.classList.add('nav-onscroll');
+
+      // for (let element of elements) {
+      //   element.classList.add('text-dark');
+      // }
+    } else {
+      header.classList.remove('header--onscroll-down');
+      logoText.classList.remove('text-dark');
+      navLinks.classList.remove('nav-onscroll');
+      console.log('scrollUp');
+    }
+    this.currentPosition = scroll;
+  };
 }
